36133e5f37fba26f58ce8dbfcee29b5fa3431e33
michel <michelsfranco@gmail.com>
2022-12-04 17:39:04 -0800

    sixth commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).scroll(function(){
+     const y = $("html").scrollTop();
+
+     y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
+
+
+})
+
+
+
+$(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
